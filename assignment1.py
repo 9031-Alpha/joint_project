953b7472a42f203209ae34c311064d327f2f0b14
oigbasan <oigbasan@uwo.ca>
2018-09-22 20:34:50 -0400

    part B included

--- assignment1.py
+++ assignment1.py
@@ -5,7 +5,7 @@ Created on Thu Sep 20 00:14:20 2018
 @author: owner
 """
 """ Assignment 1: Alpha Group (7) """
-
+"""
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percentage of your salary to save, as a decimal: " ))
 total_cost = float(input("Enter the cost of your dream home: "))
@@ -17,7 +17,22 @@ months= 0
 while current_savings < portion_down_payment :
     current_savings += (monthly_salary * portion_saved) + (current_savings * r / 12)
     months += 1
-print("Number of months: " + str(months))
+print("Number of months: " + str(months))"""
 
 
 """ Part B """
+annual_salary = float(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percentage of your salary to save, as a decimal: " ))
+total_cost = float(input("Enter the cost of your dream home: "))
+semi_annual_raise=float(input("Enter the semi-annual raise, as a decimal: " ))
+monthly_salary = annual_salary / 12
+portion_down_payment = 0.25 * total_cost
+current_savings = 0
+r = 0.04 # annual rate of return
+months= 0
+while current_savings < portion_down_payment :
+    if months >0 and months % 6 == 0:
+        monthly_salary *=  (1 + semi_annual_raise)
+    current_savings += (monthly_salary * portion_saved) + (current_savings * r / 12)
+    months += 1
+print("Number of months: " + str(months))
