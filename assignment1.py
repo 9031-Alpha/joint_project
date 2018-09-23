bd3891e69cdd9a4a336b5342773a16af799d0e85
oigbasan <oigbasan@uwo.ca>
2018-09-22 20:50:03 -0400

    second commit

--- assignment1.py
+++ assignment1.py
@@ -34,3 +34,5 @@ while current_savings < portion_down_payment :
     current_savings += (monthly_salary * portion_saved) + (current_savings * r / 12)
     months += 1
 print("Number of months: " + str(months))
+
+""" Part C """
