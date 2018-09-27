2a436099656531c2716bfcb987cd8affb196e55b
oigbasan <oigbasan@uwo.ca>
2018-09-27 14:44:26 -0400

    last commit

--- assignment1.py
+++ assignment1.py
@@ -113,6 +113,7 @@ print("The number of combinations of n objects taken r is",fact_n/(fact_r*fact_k
 
 """ Section 2 -C """
 # Binomial sum
+# Taking user input and initializing variables
 x=int(input("Enter the value of x: "))
 n=int(input("Enter the value of n: "))
 a=int(input("Enter the value of a: "))
