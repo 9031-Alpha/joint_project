0847db6fa6158e6ce521c5f99eab19c094b536c5
oigbasan <oigbasan@uwo.ca>
2018-10-31 01:28:34 -0400

    B2 and B3 commit

--- WordGame.py
+++ WordGame.py
@@ -4,7 +4,7 @@ import math
 import copy
 
 WORDLIST_FILENAME = "words.txt"
-
+vowels = ['a','e','i','o','u']
 def load_words():
     """
     Returns a list of words. Words are strings of lowercase letters.
@@ -31,7 +31,7 @@ def get_word_score(word, n):
                               'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1,
                               'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
                               's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8,
-                              'y': 4, 'z': 10}
+                              'y': 4, 'z': 10, '*':0}
     score = 0
     for i in word:
         score += SCRABBLE_LETTER_VALUES[i]
@@ -56,7 +56,7 @@ def display_hand(hand):
     displayed = ''
     for i in hand.keys():
         displayed += i*hand[i]
-    print (' '.join(displayed))
+    return (' '.join(displayed))
 
 
 def deal_hand(n):
@@ -84,11 +84,13 @@ def update_hand(hand, spelled_word):
 
 
 def is_valid_word(word):
-    vowels = ['a','e','i','o','u']
+    ''' word(string): word that is played using the current hand. It takes into account the possibility of a wildcard
+        If a wildcard is found, it replaces it with different vowels and checks if it forms a valid word '''
+        
     count = 0
     if '*' in word:     # Check if word played has a wild card in it
         i=word.find('*')
-        while count < len(vowels):  # replace wild card with vowels and check if word exist
+        while count < len(vowels):  # replace wild card with vowels and check if word exist, if it does break out of loop 
             check=list(word)
             check[i]=vowels[count]
             count +=1
@@ -100,8 +102,32 @@ def is_valid_word(word):
         return word in words  
    
         
-
-
+def play_hand():
+    total_score = 0
+    n=int(input('Enter initial hand size: '))
+    current_hand =  deal_hand(n)
+    print('Current Hand: ',display_hand(current_hand))
+    while len(current_hand)>0:
+        word = input('Enter word, or "!!" to indicate that you are finished: ')
+        if is_valid_word(word):
+            total_score += get_word_score(word,n)
+            print(word +' earned '+str(get_word_score(word,n)) + ' points. Total '+str(total_score)+' points')
+            print('')
+            current_hand = update_hand(current_hand,word)
+            print('Current Hand: ',display_hand(current_hand))
+        elif not is_valid_word(word) and (word !='!!'):
+            print('This is not a valid word. Please choose another word')
+            current_hand = update_hand(current_hand,word)
+            print('Current Hand: ',display_hand(current_hand))
+        elif word == '!!':
+            print('Total score: ',total_score )
+            break
+        n=len(current_hand)
+    if len(current_hand) == 0:
+        print('Ran out of letters. Total score: ',total_score)
+        
+            
+            
 
 
 
