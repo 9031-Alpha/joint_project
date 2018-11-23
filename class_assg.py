88b841e2fb36f35bbf57d37c72a22acc2582a80a
Mohamed Fahmy <43506071+M0hamed-Fahmy@users.noreply.github.com>
2018-11-23 02:50:04 -0500

    Update class_assg.py

--- class_assg.py
+++ class_assg.py
@@ -1,7 +1,8 @@
-
 from datetime import date
 
 authorList = 'author.txt'
+bookList = 'books.txt'
+
 def load_words():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
@@ -24,9 +25,9 @@ def get_authors():
 
 class Author():
     def __init__(self,name=None,dob=None,nationality=None):
-        self.name = name
-        self.dob = dob
-        self.nationality = nationality
+        self.name = str(name)
+        self.dob = str(dob)
+        self.nationality = str(nationality)
     
     def get_age(self):
         today = date.today()
@@ -76,7 +77,9 @@ class Author():
         for line in doc:
             if name in line:
                 break
-        return line[:-1]
+        #return Author(name=line[:-1])
+        line=line.split(',')
+        return Author(name=line[0],dob=line[1],nationality=line[2])
     
     def search_by_nationality(nationality):
         inFile = open(authorList, 'r')    
@@ -87,25 +90,113 @@ class Author():
             if nationality in line:
                 pos = line.find(',')
                 temp.append(line[:pos])
-        return temp
-                
-     
-          
-          
+        return Author(temp)
+
+
+
     
-     
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
+class Book():
+    def __init__(self,book_name=None,author_name=None,publisher_name=None):
+        self.book_name = str(book_name)
+        self.author_name = str(author_name)
+        self.publisher_name = str(publisher_name)
+        self.author = Author.search_author(author_name)
+    
+    def __str__(self):
+        return 'Book Info- Book Name:'+ self.book_name +' Publisher:'+ self.publisher_name + print(author)
+    
+    def __add__(self,other):
+        Book.__init__(self,book_name=None,author_name=None,publisher_name=None)
+        self.book_name = other(0)
+        self.author_name = other(1)
+        self.publisher_name = other(2)
+        
+    def load_books():
+        inFile = open(bookList, 'r')    
+        doc = inFile.readlines()
+        book_list = [x.strip() for x in doc]
+        return book_list
+    
+    def book_to_text(self):
+        file = open('books.txt','w+')
+        file.write(self.book_name+','+self.author_name+','+self.publisher_name +'\n')
+        
+    def del_book(name):
+        inFile = open(bookList, 'r')    
+        doc = inFile.readlines()
+        print (doc)
+        inFile.close()
+        inFile = open(bookList, 'w')
+        for line in doc:
+            if name not in line:
+                inFile.write(line)
+        inFile.close()
+        
+    def search_book(name):
+        ''' name(string) of the book to be searched. This function returns 
+        the details of the book. This function has to be called as a method of 
+        the class when used outside the class '''
+        
+        inFile = open(bookList, 'r')    
+        doc = inFile.readlines()
+        inFile.close()
+        for line in doc:
+            if name in line:
+                break
+        line=line.split(',')
+        return Book(book_name=line[0],author_name=line[1],publisher_name=line[2])
+    
+    def search_by_author(name=None, nationality=None, age=None):
+        inFile = open(bookList, 'r')    
+        doc = inFile.readlines()
+        inFile.close()
+        temp = []
+        if name != None:
+            for line in doc:
+                if name in line:
+                    pos = line.find(',')
+                    temp.append(line[:pos])
+        if nationality != None:
+            name = Author.search_by_nationality(nationality)
+            for line in doc:
+                for line2 in name:
+                    if line2 in line:
+                        pos = line.find(',')
+                        if line[:pos] not in temp:
+                            temp.append(line[:pos])
+        if age != None:
+            name = Author.search_by_age(age)
+            for line in doc:
+                for line2 in name:
+                    if line2 in line:
+                        pos = line.find(',')
+                        if line[:pos] not in temp:
+                            temp.append(line[:pos])
+                    
+            
+author = Author.search_author('segun')
+print(type(author))
+print(author)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
